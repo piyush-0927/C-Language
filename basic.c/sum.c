24905646600034713f126a9078208d9cee3427b8
piyush-0927 <lakhan0658@gmail.com>
2026-03-29 10:28:10 +0530

    add sum.c

---
+++ basic.c/sum.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main () {
+    // Arithmetic operator (+,-,*,/,%)
+    int a,b;
+    printf("enter two number : ");
+    scanf("%d %d",&a, &b);
+    printf("sum is : %d\n",a+b);
+    return 0;
+}
