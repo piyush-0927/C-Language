f4d987c2465a2cf41959691f2f7c94954f50302e
piyush-0927 <lakhan0658@gmail.com>
2026-03-30 16:10:38 +0530

    fixed conditional

--- basic.c/conditional.c
+++ basic.c/conditional.c
@@ -4,7 +4,7 @@ int main () {
     int age;
     printf("enter number : ");
     scanf("%d",&age);
-// use if else , if,if,if , else if, else if, else 
+    // if, else if, if, else 
     if(age < 13) {
         printf("child\n")
     } else if(age >= 13 && age < 18) {
@@ -13,6 +13,4 @@ int main () {
         printf("adult\n");
     }
 
-    // ternary operators
-    age >= 18 ? printf("adult\n") : printf("not adult");
 }
