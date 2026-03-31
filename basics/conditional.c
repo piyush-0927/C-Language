2769a1f0a782eb7bc2e7b689f6eb8e4a0989fd75
piyush-0927 <lakhan0658@gmail.com>
2026-03-31 16:52:11 +0530

    added for_loop

---
+++ basics/conditional.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main () {
+    int age;
+    printf("enter number : ");
+    scanf("%d",&age);
+    // if, else if, if, else 
+    if(age < 13) {
+        printf("child\n")
+    } else if(age >= 13 && age < 18) {
+        printf("Teenager\n");
+    } else {
+        printf("adult\n");
+    }
+
+}
---
+++ basics/for_loop.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main () {
+    // i=iterator/counter (i, j, k);
+    // i++ use then increase ++i increase then use;
+    for(int i=1; i<=10; i++) {
+        printf("%d\n",i);
+        printf("Hello!\n");   // blocks of code in statement
+    }
+}
