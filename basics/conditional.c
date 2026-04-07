88a2b7f4793ed9933d4a4ed5b585f966cdb22d4b
piyush-0927 <lakhan0658@gmail.com>
2026-04-07 12:30:50 +0530

    update conditional

--- basics/conditional.c
+++ basics/conditional.c
@@ -6,11 +6,11 @@ int main () {
     scanf("%d",&age);
     // if, else if, if, else 
     if(age < 13) {
-        printf("child\n")
+        printf("child\n");
     } else if(age >= 13 && age < 18) {
         printf("Teenager\n");
     } else {
         printf("adult\n");
     }
-
+    return 0;
 }
